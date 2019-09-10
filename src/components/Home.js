3d300b6746644d45d4e177de45332416c0d1571c
raywoolman <rayjwoolman@gmail.com>
2019-09-10 14:21:45 +0200

    installed dependencies and coded landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,31 @@
-import React from 'rgit eact'
+import React from 'react'
 import rightArrow from '../images/circled-right-2.png'
+import logo from '../images/MYtineraryLogo.png'
+import homeIcon from '../images/homeIcon.png'
 import '../style/home.css'
+import '../style/main.css'
+
 const Home = () => {
     return (
-        <div className="container">
-            <p>
-                Find your perfect trip, designed by insiders who know and love their cities
+        <div className="viewport">
+            <div className="mainContainer">
+                <img src={logo} alt="" className="logoImg" />
+                <p>
+                    Find your perfect trip, designed by insiders who know and love their cities
             </p>
-            <div className="startBrowsingWrapper">
-                <p className="bold">Start Browsing</p>
-                <img src={rightArrow} className="mediumIcon" alt="" />
-            </div>
-            <div className="byoIniteraryWrapper">
-                <p>Want to build your own MYtinereary</p>
-                <br />
-            </div>
-            <div className="rowJustifyBetween">
-                <a href="/login">Login</a>
-                <a href="/signup">Create Account</a>
+                <div className="startBrowsingWrapper">
+                    <p className="bold">Start Browsing</p>
+                    <img src={rightArrow} className="mediumIcon" alt="" />
+                </div>
+                <div className="byoIniteraryWrapper">
+                    <p>Want to build your own MYtinereary</p>
+                    <br />
+                </div>
+                <div className="rowJustifyBetween">
+                    <a href="/login">Login</a>
+                    <a href="/signup">Create Account</a>
+                </div>
+                <img src={homeIcon} alt="" className="homeIcon" />
             </div>
         </div>
     )
