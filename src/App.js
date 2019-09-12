a787fdc76859474a758ee104f3b6e719661b0ee2
raywoolman <rayjwoolman@gmail.com>
2019-09-12 10:46:03 +0200

    Made shells for components login, create account, sign up, itineraries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
-import logo from "./images/MYtineraryLogo.png";
-import "./App.css";
-import Home from './components/Home'
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './style/main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+//routes:
+import Home from './components/Home'
+import Cities from './components/Cities'
+import Itineraries from './components/Itineraries'
+import CreateAccount from './components/CreateAccount'
+import Login from './components/Login'
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App" >
+          {/* insert static content here */}
+          <div className="viewport">
+            <Route exact path='/' component={Home} />
+            <Route path='/cities' component={Cities} />
+            <Route path='/itineraries' component={Itineraries} />
+            <Route path='/login' component={Login} />
+            <Route path='/createaccount' component={CreateAccount} />
+          </div>
+          <Home />
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Cities.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Cities extends Component {
+    render() {
+        return (
+            <div>Cities</div>
+        )
+    }
+}
+
+export default Cities;
