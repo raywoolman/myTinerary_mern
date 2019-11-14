462d33b6011411fc5c76610d59e43ac829fdd4d9
raywoolman <rayjwoolman@gmail.com>
2019-11-14 10:37:43 +0100

    fixed npm clashes and bugs. API fetch is working correctly and now will reinstall react-strap

--- client/src/components/Cities.js
+++ client/src/components/Cities.js
@@ -2,12 +2,6 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as actions from '../store/actions/cityActions';
 
-
-import 'antd/es/input/style/index.css';
-import 'antd/es/select/style/index.css';
-import 'antd/es/cascader/style/index.css';
-import { Card } from 'antd';
-
 class Cities extends Component {
 
   constructor(props) {
@@ -30,18 +24,7 @@ class Cities extends Component {
         {this
             .props
             .cities
-            .map(city => 
-            <Card key={ city._id } 
-              cover={
-                <img 
-                  src={ city.image }>
-                </img> } 
-              title={ city.name } 
-              extra={ 
-                <a href={ city.image }>More
-                </a>
-            }>
-            </Card>)}
+            .map(city =><div key={ city._id }>{ city.name }</div>)}
       </div>
     )
   }
